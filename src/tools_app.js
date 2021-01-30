00bb9758ced46fa7a1dc58191dae9075a2021aaf
afterloe <605728727@qq.com>
2021-01-30 23:55:24 +0800

    feat: left控制器活化

--- src/tools_app.js
+++ src/tools_app.js
@@ -131,7 +131,7 @@ class NavConfigApp extends React.Component {
                     );
                 })}
             </div>
-            )
+        )
     })
 
 
@@ -171,7 +171,21 @@ class ToolsApp extends React.Component {
         this.state = {};
     }
 
+    renderMainView = (activeCode = 0) => {
+        switch (activeCode) {
+            case 0:
+                return <NavConfigApp/>
+            case 3:
+                return <VideoManagerApp/>
+        }
+    };
+
+    changeView = (activeCode = 0) => {
+        this.setState({activeCode});
+    }
+
     render() {
+        const {activeCode = 0} = this.state;
         return (
             <div className="tools_app">
                 <div className="left_control">
@@ -181,24 +195,40 @@ class ToolsApp extends React.Component {
                     <div className="line"/>
                     <div>
                         <div className="title">字典</div>
-                        <div>
-                            <div className="item">导航配置</div>
-                            <div className="item">标签管理</div>
-                            <div className="item">目录配置</div>
+                        <div onClick={e => this.changeView(0)}
+                             className={activeCode === 0 ? "item active" : "item"}>导航配置
+                        </div>
+                        <div onClick={e => this.changeView(1)}
+                             className={activeCode === 1 ? "item active" : "item"}>标签管理
+                        </div>
+                        <div onClick={e => this.changeView(2)}
+                             className={activeCode === 2 ? "item active" : "item"}>目录配置
                         </div>
                     </div>
                     <div className="line"/>
                     <div>
                         <div className="title">内容</div>
-                        <div className="item active">视频管理</div>
-                        <div className="item">图片管理</div>
-                        <div className="item">小说管理</div>
-                        <div className="item">直播管理</div>
+                        <div onClick={e => this.changeView(3)}
+                             className={activeCode === 3 ? "item active" : "item"}>视频管理
+                        </div>
+                        <div onClick={e => this.changeView(4)}
+                             className={activeCode === 4 ? "item active" : "item"}>图片管理
+                        </div>
+                        <div onClick={e => this.changeView(5)}
+                             className={activeCode === 5 ? "item active" : "item"}>小说管理
+                        </div>
+                        <div onClick={e => this.changeView(6)}
+                             className={activeCode === 6 ? "item active" : "item"}>直播管理
+                        </div>
                     </div>
                     <div className="line"/>
                     <div>
-                        <div className="title">用户</div>
-                        <div className="item">用户管理</div>
+                        <div onClick={e => this.changeView(7)}
+                             className={activeCode === 7 ? "item active" : "item"}>用户
+                        </div>
+                        <div onClick={e => this.changeView(8)}
+                             className={activeCode === 8 ? "item active" : "item"}>用户管理
+                        </div>
                     </div>
                     <div className="bottom-view">
                         <div>
@@ -206,8 +236,7 @@ class ToolsApp extends React.Component {
                         </div>
                     </div>
                 </div>
-                {/*<VideoManagerApp />*/}
-                <NavConfigApp />
+                {this.renderMainView(activeCode)}
             </div>
         );
     }
