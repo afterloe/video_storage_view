af4c0f7660040c4459141f1b2e32c94ac1c25ba5
liumin <liumin@ascs.tech>
2022-09-21 20:22:11 +0800

    fix: 用户注册页面不反馈

---
+++ src/register.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * 用户注册
+ */
+class RegisterUserApp extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div className="wrap">
+
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<RegisterUserApp />, document.getElementById("app"));
